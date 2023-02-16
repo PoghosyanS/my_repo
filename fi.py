28b61727ca2b5ed575a72ce55b7579d473c9f37e
Poghosyan <poghosyan2014@bk.ru>
2023-02-16 14:23:18 +0400

    comm

---
+++ fi.py
@@ -0,0 +1,13 @@
+import os
+def func(path,file_name):
+    for i in os.listdir(path):
+        if os.path.isdir(path + "/" + i):
+            func(path + "/" + i,file_name)
+        if os.path.isfile(path + "/" + i):
+            if i == file_name:
+                print(path +"/" +file_name)
+        if os.path.isdir(path + "/" + i):
+            if i == file_name:
+                print(path + "/" +file_name)
+
+print(func("/home/simon/tasks/bash","file1.txt"))
