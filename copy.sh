953d0796004e3eeb3b59b1aa10c537d5fe953c8e
Poghosyan <poghosyan2014@bk.ru>
2023-02-19 13:15:59 +0400

    my copy

--- copy.sh
+++ copy.sh
@@ -7,20 +7,22 @@ function my_copy(){
          
 	#copy for two files
 	
-        if [ -f $first_argument ]  && [ ! -e $second_argument ] && [ ! -d $second_argument ] && [ $# == 2 ]
+        if [ -f $first_argument ] && [ ! -d $second_argument ] && [ $# == 2 ]
 	then
-	echo $?
+
 	cat  $first_argument >> $second_argument
 	
 	#copy for directory
 
 	elif [  -d ${!#} ]
 	then 
-	echo $?
+
+
 	for file in "$@"
 	do
 		if [ -f "$file" ] && [ $# -gt 2 ]
 		then
+
 		cat "$file" > "${!#}/$file"
 		fi
 	done
