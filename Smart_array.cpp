7c5850af5ebc6825106373c741167eefe8a4c68a
Poghosyan <poghosyan2014@bk.ru>
2023-04-04 01:22:36 +0400

    homework

---
+++ Smart_array.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+
+class Smart_array{
+
+	int size;
+	int *arr;
+
+	public:
+		
+	Smart_array(int size){
+		this->size = size;
+		arr = new int[size];
+	}
+
+	~Smart_array(){
+		delete [] arr;
+	}
+
+	void make_arr(){
+		for (int i = 0;i < size;i++){
+			arr[i] = rand() % 20;
+		}
+	}
+
+	void print_arr(){
+		for (int i = 0;i < size;i++){
+			std::cout<<arr[i]<<std::endl;
+		}
+	}
+
+};
+
+int main()
+{
+	Smart_array arr(10);
+	arr.make_arr();
+	arr.print_arr();
+
+	return 0;
+}
+
+	
